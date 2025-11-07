76618a671dd3356c3564816b9c1e965cedb71921
violetmoorway-9942 <violetmoorway@gmail.com>
2025-11-07 13:41:44 +0100

    omo

--- app/index.js
+++ app/index.js
@@ -1,5 +1,15 @@
-import { Redirect } from 'expo-router';
+import { useEffect } from 'react';
+import { router } from 'expo-router';
+import { View, ActivityIndicator } from 'react-native';
 
 export default function Index() {
-  return <Redirect href="/(tabs)/" />;
+  useEffect(() => {
+    router.replace('/(tabs)/index');
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ActivityIndicator />
+    </View>
+  );
 }
